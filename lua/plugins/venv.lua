f02edc080e71cc969a1aa483c156dcc51be5c25d
Emre <e@emre.pm>
2024-03-06 09:31:33 +0000

    Add linux path for poetry

--- lua/plugins/venv.lua
+++ lua/plugins/venv.lua
@@ -1,11 +1,17 @@
 return {
   "linux-cultist/venv-selector.nvim",
-  opts = {
-    name = { "venv", ".venv" },
-    auto_refresh = true,
-    search_workspace = false,
-    poetry_path = "~/Library/Caches/pypoetry/virtualenvs",
-  },
+  opts = function()
+    local poetry_path = "~/.cache/pypoetry/virtualenvs"
+    if vim.loop.os_uname().sysname == "Darwin" then
+      poetry_path = "~/Library/Caches/pypoetry/virtualenvs"
+    end
+    return {
+      name = { "venv", ".venv" },
+      auto_refresh = true,
+      search_workspace = false,
+      poetry_path = poetry_path,
+    }
+  end,
   dependencies = { "neovim/nvim-lspconfig", "nvim-telescope/telescope.nvim", "mfussenegger/nvim-dap-python" },
   keys = {
     { "<leader>vs", "<cmd>VenvSelect<cr>" },
