6830306f51562109838ac70ab64028a667ca9cb7
Emre <e@emre.pm>
2024-03-24 23:35:45 +0300

    Update telescope keybindings

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -24,15 +24,15 @@ return {
         desc = "Switch Buffer",
       },
       { "<leader>:", "<cmd>Telescope command_history<cr>", desc = "Command History" },
-      { "<leader><space>", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
+      { "<leader><space>", "<cmd>Telescope git_files<cr>", desc = "Git Files" },
       -- find
       { "<leader>fb", "<cmd>Telescope buffers sort_mru=true sort_lastused=true<cr>", desc = "Buffers" },
-      { "<leader>ff", "<cmd>Telescope live_grep<cr>", desc = "Live Grep" },
+      { "<leader>ff", "<cmd>Telescope find_files<cr>", desc = "Find Files" },
       { "<leader>fr", "<cmd>Telescope oldfiles<cr>", desc = "Recent" },
       -- git
-      { "<leader>fg", "<cmd>Telescope git_files<CR>", desc = "Git Files" },
       { "<leader>fc", "<cmd>Telescope git_commits<CR>", desc = "commits" },
       -- search
+      { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Grep Files" },
       { '<leader>s"', "<cmd>Telescope registers<cr>", desc = "Registers" },
       { "<leader>sa", "<cmd>Telescope autocommands<cr>", desc = "Auto Commands" },
       { "<leader>sb", "<cmd>Telescope current_buffer_fuzzy_find<cr>", desc = "Buffer" },
